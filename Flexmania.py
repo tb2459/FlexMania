ff405888d27d457c42da18dd7ea4b449e880fcc5
tb2459 <87879969+tb2459@users.noreply.github.com>
2023-02-25 18:21:29 -0500

    Update Flexmania.py
    Adding a commit regarding with a basic function controlling a particular LED within a matrix.

--- Flexmania.py
+++ Flexmania.py
@@ -1,6 +1,9 @@
+import setup
 def setupIMU():
   pass
-def setupLEDMatrix():
-  pass
+def setupLEDMatrix(X_position, Y_position, intensity): #This turns on an LED at a given position with a given intensity for two seconds and then turns it off
+  setup.display.pixel(X_position, Y-position, intensity)
+  setup.time.sleep(2)
+  setup.display.fill(0)
 def setupMPU6050():
   pass
