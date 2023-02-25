8f0484a22254182d4819cf441f6945afb638fa44
tb2459 <87879969+tb2459@users.noreply.github.com>
2023-02-24 20:50:44 -0500

    Update setup.py
    The driver setup library has been committed

--- setup.py
+++ setup.py
@@ -1,1 +1,7 @@
-
+#This is the library setup for the driver 
+import board
+import busio
+import time
+from adafruit_is31fl3731.matrix import Matrix as Display
+i2c = busio.I2C(board.SCL, board.SDA)
+display = Display(i2c)
