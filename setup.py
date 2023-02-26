b0fb9c67b5b77171460f51631d87d1d565a5bbc4
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-02-25 22:19:04 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,3 +5,9 @@ import time
 from adafruit_is31fl3731.matrix import Matrix as Display
 i2c = busio.I2C(board.SCL, board.SDA)
 display = Display(i2c)
+
+
+#This is the library setup for the MPU 6050
+from mpu6050 import mpu6050
+mpu = mpu6050(0x68)
+
