567c177366912dbb20d978a65e032d3863c5a4b8
tb2459 <87879969+tb2459@users.noreply.github.com>
2023-02-25 19:24:48 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,5 +3,5 @@ import Flexmania
 #This is a basic example of giving x,y coordinates for the matrix LED and then applying a certain level of intensity for that particular LED 
 x = int(input("Which x coordinate would you like to choose?"))
 y = int(input("Which x coordinate would you like to choose?"))
-intensity = int(input("Which x coordinate would you like to choose?"))
+intensity = int(input("Choose a light intensity ranging from 0 and 255?"))
 Flexmania.setupLEDMatrix(x,y,intensity)
