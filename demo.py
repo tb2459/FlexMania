220687bd6d92460544a3b94652cd3996d1f20b57
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-02-25 22:24:15 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,3 +5,8 @@ x = int(input("Which x coordinate would you like to choose?"))
 y = int(input("Which x coordinate would you like to choose?"))
 intensity = int(input("Choose a light intensity ranging from 0 and 255?"))
 Flexmania.setupLEDMatrix(x,y,intensity)
+
+
+#This is an example of the data that can be gathered from the MPU6050 which would be used to determine the position of the device
+axis = str(input("Select an axis x, y, or z."))
+Flexmania.setupMPU6050(axis)
