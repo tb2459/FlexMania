6e7d4bea5ac17ddb895226137a42eb1045d3fcb2
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-07 15:43:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import board
 import busio
 import time
+from gpiozero import Button
 from adafruit_is31fl3731.matrix import Matrix as Display
 i2c = busio.I2C(board.SCL, board.SDA)
 display = Display(i2c)
