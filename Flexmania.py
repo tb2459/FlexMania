acc428bde50a774527b7f1c88b0a210bf61da377
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-07 16:16:11 -0400

    Update Flexmania.py

--- Flexmania.py
+++ Flexmania.py
@@ -5,11 +5,11 @@ def setupLEDMatrix(X_position, Y_position, intensity): #This turns on an LED at 
   setup.display.pixel(X_position, Y-position, intensity)
   setup.time.sleep(2)
   setup.display.fill(0)
-def setupMPU6050(axis): #This prints the value at whichever selected axis
+def read_MPU6050(axis): #This prints the value at whichever selected axis
   accel_data = mpu.get_accel_data()
   return(accel_data[axis])
-def setupArcadeButtons():
+def read_ArcadeButtons():
   
-def setupKeypad():
+def read_Keypad():
   
-def setupLCD():
+def write_LCD():
