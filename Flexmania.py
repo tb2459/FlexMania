e1ab21dd6551f5a28e392824ceb6a58a587a72ed
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-07 16:31:04 -0400

    Update Flexmania.py

--- Flexmania.py
+++ Flexmania.py
@@ -9,7 +9,16 @@ def read_MPU6050(axis): #This prints the value at whichever selected axis
   accel_data = mpu.get_accel_data()
   return(accel_data[axis])
 def read_ArcadeButtons():
-  
+  if up_button.is_pressed:
+    return('up')
+  elif down_button.is_pressed:
+    return('down')
+  elif left_button.is_pressed:
+    return('left')
+  elif right_button.is_pressed:
+    return('right')
+
 def read_Keypad():
   
 def write_LCD():
+  
