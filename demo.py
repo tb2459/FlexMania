890c4dd6ca2cfdebd486b0f7742b05a8f4c28efd
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-09 21:45:00 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,6 +9,29 @@ y = int(input("Which x coordinate would you like to choose?"))
 intensity = int(input("Choose a light intensity ranging from 0 and 255?"))
 Flexmania.setupLEDMatrix(x,y,intensity)
 
+on_off_count = 0
+start_stop_count = 0
+
+def on_off()
+  global on_off_count
+  While True:
+    if Flexmania.on_off() & ((on_off_count % 2) == 0 | on_off_count  == 0) :
+      on_off_count += 1
+      Flexmania.turn_on_board()
+    else:
+      on_off_count += 1
+      Flexmania.turn_off_board()
+  
+def start_stop()
+  global start_stop_count
+  While True:
+    if Flexmania.on_off() & ((start_stop_count % 2) == 0 | start_stop_count  == 0) :
+      start_stop_count += 1
+      Flexmania.start)
+    else:
+      start_stop_count += 1
+      Flexmania.off()
+
 
 def read_MPU():
   x_axis = Flexmania.read_MPU6050('x')
@@ -27,7 +50,11 @@ def update_board():
 
 
 
+read_on_off_thread = Thread(target = on_off)
+read_on_off_thread.start()
 
+read_start_stop_thread = Thread(target = start_stop)
+read_start_stop_thread.start()
 
 read_MPU6050_thread = Thread(target = read_MPU)
 read_MPU6050_thread.start()
