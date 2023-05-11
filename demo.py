de0475720e67ce7f4c84cf8af71d91cc8d606739
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-10 21:27:16 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -7,6 +7,64 @@ from queue import Queue
 on_off_count = 0
 start_stop_count = 0
 
+blue_led = {
+  0:1, 
+  1:2,
+  2:3,
+  3:4,
+  4:5,
+  5:6,
+  6:7,
+  7:8,
+  7,1
+}
+green_led = {
+  8:2,
+  9:3,
+  10:4,
+  11:5,
+  12:6,
+  13:7,
+  13:4,
+  15:8,
+  15:3
+}
+
+enemy_blue_track = {
+  5:6,
+  7:1,
+  7:8,
+  6:7,
+  3:4,
+  0:1,
+  1:2,
+  2:3
+}
+enemy_green_track = {
+  13:7,
+  15:3,
+  15:8,
+  13:4,
+  11:5,
+  8:2,
+  9:3,
+  10:4
+}
+
+
+
+def enemy_move():
+   while True:
+      for (x,y), (x1, y2) in zip(enemy_blue_track.items(), enemy_green_track.items()):
+        Flexmania.light_pixel(x, y, 255)
+        Flexmania.light_pixel(x1, y1, 255)
+        time.sleep(2)
+
+
+
+
+
+
 def on_off_board():
   global on_off_count
   while True:
