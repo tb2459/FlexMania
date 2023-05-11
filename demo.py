bfbfd21cc81a080510cf20d4a866ab774106f75c
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-10 21:04:12 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,11 +3,6 @@ from threading import Thread, Lock, Event
 from time import sleep
 from queue import Queue
 
-#This is a basic example of giving x,y coordinates for the matrix LED and then applying a certain level of intensity for that particular LED 
-x = int(input("Which x coordinate would you like to choose?"))
-y = int(input("Which x coordinate would you like to choose?"))
-intensity = int(input("Choose a light intensity ranging from 0 and 255?"))
-Flexmania.setupLEDMatrix(x,y,intensity)
 
 on_off_count = 0
 start_stop_count = 0
