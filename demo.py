16166f299e8441458d8355b283f7d61de77e14f7
Bkon243 <98844170+Bkon243@users.noreply.github.com>
2023-05-14 15:15:26 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,8 +9,7 @@ import sys
 
 on_off_count = 0
 start_stop_count = 0
-index_i = 0
-index_k = 1
+
 score = 0
 
 
@@ -27,19 +26,6 @@ joystick_queue = []
 arcade_queue = []
 controller = []
 
-
- Points: 
-  {
-  1:[[0,1],[8,2]]
-  2:[[1,2],[9,3]]
-  3:[[2,3],[10,4]]
-  4:[[3,4],[11,5]]
-  5:[[4,5],[12,6]]
-  6:[[5,6],[13,7]]
-  7:[[6,7],[13,4]]
-  8:[[7,8],[15,8]]
-  9:[[7,1],[15,3]]
-}
 blue_led = [
     {0:[0,1]}, 
     {1:[1,2]},
@@ -85,71 +71,11 @@ enemy_green_track = [
 ]
 
 
-
-
-
-
 mpu_queue = []
 joystick_queue = []
 arcade_queue = []
 
-def joystick():
-  while True:
-    if((index_i == 0) and (index_k == 1)):
-      if(setup.digitalValue_Pressure_Sensor1<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 3
-        index_k = 4
-        Flexmania.light_pixel(index_i,index_k,127)
-      if(setup.digitalValue_Pressure_Sensor4<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 1
-        index_k = 2
-        Flexmania.light_pixel(index_i,index_k,127)
-    elif((index_i == 1) and (index_k == 2)):
-      if(setup.digitalValue_Pressure_Sensor1<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 4 
-        index_k = 5
-        Flexmania.light_pixel(index_i,index_k,127)
-      if(setup.digitalValue_Pressure_Sensor4<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 2 
-        index_k = 3
-        Flexmania.light_pixel(index_i,index_k,127)
-      if(setup.digitalValue_Pressure_Sensor2<300):
-        setup.display.pixel(index_i,index_k,0)
-        index_i = 0
-        index_k = 1
-        Flexmania.light_pixel(index_i,index_k,127)
-     elif((index_i == 2) and (index_k == 3)):
-      if(setup.digitalValue_Pressure_Sensor1<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 5
-        index_k = 6
-        Flexmania.light_pixel(index_i,index_k,127)
-      if(setup.digitalValue_Pressure_Sensor2<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 1
-        index_k = 2
-        Flexmania.light_pixel(index_i,index_k,127)
-     elif((index_i == 3) and (index_k == 4)):
-      if(setup.digitalValue_Pressure_Sensor1<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 6 
-        index_k = 7
-        Flexmania.light_pixel(index_i,index_k,127)
-      if(setup.digitalValue_Pressure_Sensor4<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 4 
-        index_k = 5
-        setup.display.pixel(index_i,index_k,127)
-      if(setup.digitalValue_Pressure_Sensor3<300):
-        Flexmania.light_pixel(index_i,index_k,0)
-        index_i = 0
-        index_k = 1
-        Flexmania.light_pixel(index_i,index_k,127)
-        
+
 def enemy_move():
   enemy_pos_track = 0
   while True:    
@@ -263,9 +189,14 @@ def read_MPU():
     y_axis = Flexmania.read_MPU6050('y')
     x_y = [x_axis, y_axis]
     mpu_queue.append(x_y)
+    
 def read_arcade():
   while True:
     arcade_queue.append(Flexmania.read_ArcadeButtons())
+    
+def read_joystick():
+  while True:
+    joystick_queue.append(Flexmania.read_joystick())
   
 def update_score(score):
   while True:
@@ -287,6 +218,9 @@ read_MPU6050_thread.start()
 
 read_arcade_thread = Thread(target = read_arcade)
 read_arcade_thread.start()
+
+read_joystick_thread = Thread(target = read_joystick)
+read_joystick_thread.start()
 
 update_score_thread = Thread(target = update_score)
 update_score_thread.start()
